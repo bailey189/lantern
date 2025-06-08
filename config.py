5e6fb027c29acf86fcb73e7f8074fd128542756c
bailey189 <bailey189@gmail.com>
2025-06-08 10:57:59 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://lanternuser:yourpassword@localhost:5432/lanterndb'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://lanternuser:password@localhost:5432/lanterndb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
