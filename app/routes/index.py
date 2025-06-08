5f584bf96646e8cf2b98aa4dcc25d913c79d0eb9
bailey189 <bailey189@gmail.com>
2025-06-08 17:47:55 -0400

    Update index.py

--- app/routes/index.py
+++ app/routes/index.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 index_bp = Blueprint('index', __name__)
 
-@index_bp.route("/")
+@index_bp.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
