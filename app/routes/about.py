4bfb0e2b52ea85fd784ecf8b2a51fc095b2b1715
bailey189 <bailey189@gmail.com>
2025-06-09 01:13:44 -0400

    Update about.py

--- app/routes/about.py
+++ app/routes/about.py
@@ -3,5 +3,5 @@ from flask import Blueprint, render_template
 about_bp = Blueprint('about', __name__, url_prefix='/about')
 
 @about_bp.route('/')
-def about_home():
+def about():
     return render_template('about.html', title="About Lantern")
