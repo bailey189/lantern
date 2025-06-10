5e3ed94b6bf9a211476aae3db38d864c6bb44ce1
bailey189 <bailey189@gmail.com>
2025-06-09 23:04:47 -0400

    Create devices.py

---
+++ app/routes/devices.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template
+devices_bp = Blueprint('devices', __name__, url_prefix="/devices")
+
+@devices_bp.route("/")
+def devices():
+        return render_template('devices.html', title="Lantern - Scan")
+
+@devices_bp.route('/credentials', methods=['GET', 'POST'])
+def save_credentials():
+    if request.method == 'POST':
+        ip = request.form.get('ip')
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        device = Device.query.filter_by(ip_address=ip).first()
+        if device:
+            cred = Credential(device_id=device.id, username=username, password=password)
+            db.session.add(cred)
+            db.session.commit()
+            flash("Credentials saved", "success")
+        else:
+            flash("Device not found", "error")
+
+    return render_template('device_credentials.html')
