e791645a4bdd12e80a60df8820688d26bd11fde4
bailey189 <bailey189@gmail.com>
2025-06-12 01:37:24 -0400

    updated to show assets

--- app/routes/assets.py
+++ app/routes/assets.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from app.models import Asset, Credential
+from app import db
 
 assets_bp = Blueprint('assets', __name__, url_prefix="/assets")
 
 @assets_bp.route("/")
 def assets():
-        return render_template('assets.html', title="Lantern - Assets")
+    assets_list = Asset.query.order_by(Asset.last_scanned_date.desc().nullslast()).all()
+    return render_template('assets.html', title="Lantern - Assets", assets=assets_list)
 
 @assets_bp.route('/credentials', methods=['GET', 'POST'])
 def save_credentials():
@@ -13,13 +16,13 @@ def save_credentials():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        asset = asset.query.filter_by(ip_address=ip).first()
+        asset = Asset.query.filter_by(ip_address=ip).first()
         if asset:
             cred = Credential(asset_id=asset.id, username=username, password=password)
             db.session.add(cred)
             db.session.commit()
             flash("Credentials saved", "success")
         else:
-            flash("asset not found", "error")
+            flash("Asset not found", "error")
 
     return render_template('asset_credentials.html')
