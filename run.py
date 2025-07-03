d37339cf148b94898bab88c21c20fd5be4c605e1
bailey189 <bailey189@gmail.com>
2025-07-02 22:01:30 -0400

    updated port

--- run.py
+++ run.py
@@ -39,4 +39,4 @@ def validate_license():
 if __name__ == "__main__":
     if validate_license():
         print("Starting Lantern application...")
-    app.run()
+    app.run(app.run(host='0.0.0.0', port=80))
