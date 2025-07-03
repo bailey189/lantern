e5901828ae69c9af8b151dbea651ba8b1d4497cd
bailey189 <bailey189@gmail.com>
2025-07-02 21:25:57 -0400

    updated port to 80

--- run.py
+++ run.py
@@ -42,4 +42,4 @@ def validate_license():
 if __name__ == '__main__':
     if validate_license():
         app = create_app()
-        app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=80)
