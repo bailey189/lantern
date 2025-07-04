5d526ca22fc15de10139f56e5926b17fc0a16f6a
bailey189 <bailey189@gmail.com>
2025-07-04 13:26:12 -0400

    updated routes

--- app/routes/index.py
+++ app/routes/index.py
@@ -13,21 +13,18 @@ def index():
     button_text = "Initial Survey"
     return render_template('index.html', action_button_text=button_text)
 
-@index_bp.route('/survey', methods=['GET', 'POST'])
-def survey():
-    if request.method == 'POST':
-        flash("Thank you for submitting the survey!", "success")
-        return redirect(url_for('index_bp.index'))
-    return render_template('survey.html')
+@index_bp.route('/about')
+def about():
+    return render_template('about.html') 
 
-@index_bp.route('/generate_report', methods=['POST'])
+@index_bp.route('/network', methods=['POST', 'GET'])
+def network():
+    # Your logic here
+    return render_template('network.html')
+
+@index_bp.route('/report', methods=['POST'])
 def generate_report():
     flash("Report generation started (not implemented).")
-    return redirect(url_for('index_bp.index'))
-
-@index_bp.route('/wipe_system', methods=['POST'])
-def wipe_system():
-    flash("System wipe started (not implemented).")
     return redirect(url_for('index_bp.index'))
 
 @index_bp.route('/run_update', methods=['POST'])
@@ -47,6 +44,13 @@ def run_update():
     flash("Lantern update script started. The page will refresh in 30 seconds.", "success")
     return render_template('update_wait.html')
 
+@index_bp.route('/survey', methods=['GET', 'POST'])
+def survey():
+    if request.method == 'POST':
+        flash("Thank you for submitting the survey!", "success")
+        return redirect(url_for('index_bp.index'))
+    return render_template('survey.html')
+
 @index_bp.route('/update_progress')
 def update_progress():
     if os.path.exists(UPDATE_PROGRESS_FILE):
@@ -56,6 +60,8 @@ def update_progress():
         progress = "No update in progress."
     return progress, 200, {'Content-Type': 'text/plain'}
 
-@index_bp.route('/about')
-def about():
-    return render_template('about.html') 
+
+@index_bp.route('/wipe_system', methods=['POST'])
+def wipe_system():
+    flash("System wipe started (not implemented).")
+    return redirect(url_for('index_bp.index'))
